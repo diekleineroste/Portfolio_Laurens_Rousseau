a49e7267bd76fc5ea25e6cdaf96ae694b8ef0bfa
diekleineroste <laurensrousseau@gmail.com>
2024-04-09 23:12:40 +0200

    perspective to orthographic camera

--- assets/js/three.js
+++ assets/js/three.js
@@ -17,10 +17,10 @@ const sizes = {
 //scene
 const scene = new THREE.Scene();
 
-const grid = new THREE.GridHelper(200, 160, 0xff8ff8, 0x444444);
+const grid = new THREE.GridHelper(window.innerHeight * 20, 130, 0xff8ff8, 0x444444);
 scene.add(grid);
 grid.rotation.x = Math.PI * 0.5
-grid.position.set(-1.25, -3.2, -2)
+grid.position.set(-window.innerWidth / 9, -window.innerHeight / 3, -750)
 
 //model
 
@@ -31,15 +31,16 @@ gltfloader.load(urlMichelangelo.href,
         const model = object.scene
         michelangelo = model
         scene.add(model)
-        model.scale.set(-10, 10, 10)
-        if (sizes.aspect >= 1) model.position.set(((window.innerWidth / 2) / 175), -.7, 0)
-        else model.position.set(100, -.7, 0)
+        model.scale.set(-1100, 1100, 1100)
+        model.position.set((window.innerWidth / 2) - 350, -innerHeight/9, -250)
+        if (window.innerWidth > 800) model.position.z = -250
+        else model.position.z = -2000
         model.rotation.y = Math.PI * 1.45
     }
 )
 
 let woman
-const geometry = new THREE.TorusGeometry(.5, .02, 16, 100)
+const geometry = new THREE.TorusGeometry(40, 3, 16, 100)
 const material = new THREE.MeshStandardMaterial({
     color: 0xff8ff8,
     roughness: .5,
@@ -47,28 +48,29 @@ const material = new THREE.MeshStandardMaterial({
 })
 const torus = new THREE.Mesh(geometry, material)
 scene.add(torus)
-torus.rotation.x = Math.PI * 0.5
-
-const sLightTorus = new THREE.SpotLight(0xffffff, .6, 1, 1.4);
-scene.add(sLightTorus)
-sLightTorus.target = torus
+torus.rotation.x = Math.PI * 0.45
 
 gltfloader.load(urlWoman.href,
     function (object) {
         const model = object.scene
         woman = model
         scene.add(model)
-        model.scale.set(3.4, 3.4, 3.4)
 
-        if (sizes.aspect < 1) {
-            model.position.set(0, -.4, 0)
-            torus.position.set(0, 3.3, 0)
-            sLightTorus.position.set(0, 3, 0)
+        const ws = window.innerHeight / 2.3
+        const ts = window.innerHeight / 800
+
+        model.scale.set(ws, ws, ws)
+        model.position.set(0, -window.innerHeight / 13, -250)
+
+        torus.scale.set(ts, ts, ts).set(ts, ts, ts)
+        torus.position.set(0, window.innerHeight / 2.7, -250)
+
+        if (window.innerWidth > 800) {
+            model.position.z = -2000
+            torus.position.z = -2000
         }
         else {
-            model.position.x = 100
-            torus.position.x = 100
-            sLightTorus.position.x = 100
+            model.position.z = -250
         }
     }
 )
@@ -79,10 +81,11 @@ gltfloader.load(urlAngel.href,
         const model = object.scene
         angel = model
         scene.add(model)
-        model.rotation.x = Math.PI * 0.12
-        model.scale.set(0.2, 0.2, 0.2)
-        if (sizes.aspect < 1) model.position.set(100, -9, 0)
-        else model.position.set(-((window.innerWidth / 2) / 175), -9, 0)
+        const s = window.innerHeight / 42
+        model.scale.set(s, s, s)
+        model.position.set((-window.innerWidth / 4), -window.innerHeight, -250)
+        if (window.innerWidth > 800) model.position.z = -250
+        else model.position.z = -2000
     }
 )
 
@@ -92,12 +95,12 @@ gltfloader.load(urlThinker.href,
         const model = object.scene
         thinker = model
         scene.add(model)
-        const ts = (1.2 + (window.innerWidth / 350)) >= 3 ? 3 : (1.2 + (window.innerWidth / 350))
-        model.scale.set(ts, ts, ts)
-        model.rotation.y = Math.PI * .7
-        if (sizes.aspect < 1) model.position.set(-((window.innerWidth / 2) / 200), -12.6 + ((window.innerWidth - 300) / 4000), 0)
-        else model.position.set(100, -11.2, 0)
-        console.log(thinker.position.y)
+        const s = window.innerHeight / 3.4
+        model.rotation.y = Math.PI * 0.7
+        model.scale.set(s, s, s)
+        model.position.set(-window.innerWidth / 3, -window.innerHeight * 1.47, -250)
+        if (window.innerWidth > 800) model.position.z = -2000
+        else model.position.z = -250
     }
 )
 
@@ -107,30 +110,40 @@ gltfloader.load(urlLacoon.href,
         const model = object.scene
         lacoon = model
         scene.add(model)
-        model.scale.set(-0.15, 0.15, 0.15)
-        model.position.set(0, -20, 0)
+        const s = window.innerHeight / 37
+        model.scale.set(-s, s, s)
+        model.rotation.x = Math.PI * 0.1
+        model.position.set(0, -window.innerHeight * 2.15, -250)
     }
 )
 
 //lights
-const pointlight = new THREE.PointLight(0xffffff, 500, 40);
+const pointlight = new THREE.PointLight(0xffffff, 400000, 400000);
 const ambientlight = new THREE.AmbientLight(
     0xffffff,
     0.05,
 )
 scene.add(pointlight, ambientlight);
 
-pointlight.position.set(0, 10, 20);
+pointlight.position.set(0, 10, 500);
 
 //camera
-const camera = new THREE.PerspectiveCamera(
-    45,
-    sizes.width / sizes.height,
-    1,
-    100
-);
-camera.position.set(0, 0, 10);
-scene.add(camera);
+// const camera = new THREE.PerspectiveCamera(
+//     45,
+//     sizes.width / sizes.height,
+//     1,
+//     100
+// );
+const camera = new THREE.OrthographicCamera( 
+    sizes.width / - 2, 
+    sizes.width / 2, 
+    sizes.height / 2, 
+    sizes.height / - 2, 
+    0, 
+    1000
+)
+camera.position.set(0, 0, 0)
+scene.add(camera)
 
 //renderer
 const canvas = document.querySelector("#canvas");
@@ -197,7 +210,7 @@ scrollAnimations.push({
     start: 0,
     end: 101,
     func: () => {
-        camera.position.y = -lerp(0, 18, scalePercent(0, 101))
+        camera.position.y = -lerp(0, window.innerHeight * 2, scalePercent(0, 101))
     },
 })
 
@@ -219,7 +232,10 @@ document.body.onscroll = () => {
 //resize
 
 window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.left = window.innerWidth / - 2, 
+    camera.right = window.innerWidth / 2, 
+    camera.top = window.innerHeight / 2, 
+    camera.bottom = window.innerHeight / - 2, 
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -230,27 +246,26 @@ window.addEventListener('resize', () => {
 }, false)
 
 function changeModel() {
-    sizes.aspect = window.innerWidth / window.innerHeight
-    const ts = (1.2 + (sizes.aspect / 0.45)) >= 3 ? 3 : (1.2 + (sizes.aspect / 0.45))
+    // const ts = (1.2 + (window.innerWidth / 400)) >= 3 ? 3 : (1.2 + (window.innerWidth / 400))
 
-    if (sizes.aspect >= 1) {
-        michelangelo.position.x = (window.innerWidth / 2) / 175
-        woman.position.x = 100
-        torus.position.x = 100
-        sLightTorus.position.x = 100
-        thinker.position.x = 100
-        angel.position.x = -((window.innerWidth / 2) / 175)
-    }
-    else {
-        michelangelo.position.x = 100
-        woman.position.x = 0
-        torus.position.x = 0
-        sLightTorus.position.x = 0
-        thinker.position.x = -((window.innerWidth / 2) / 200)
-        angel.position.x = 100
-    }
-    thinker.scale.set(ts, ts, ts)
-    thinker.position.y = -12.6 + ((window.innerWidth - 300) / 4000)
+    // if (sizes.aspect >= 1) {
+    //     michelangelo.position.z = -250
+    //     woman.position.z = -2000
+    //     torus.position.z = -2000
+    //     sLightTorus.position.z = -2000
+    //     thinker.position.z = -2000
+    //     angel.position.x = -4
+    // }
+    // else {
+    //     michelangelo.position.z = -2000
+    //     woman.position.x = 0
+    //     torus.position.x = 0
+    //     sLightTorus.position.z = 0
+    //     thinker.position.z = -400
+    //     angel.position.z = -2000
+    // }
+    // thinker.scale.set(ts, ts, ts)
+    // thinker.position.y = -13.4 + ((window.innerWidth - 300) / 4000)
 }
 
 window.scrollTo({ top: 0, behavior: 'smooth' })
