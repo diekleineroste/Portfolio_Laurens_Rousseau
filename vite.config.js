306284ad4f689e02a6b899dfd632a09d2b70f29a
diekleineroste <laurensrousseau@gmail.com>
2024-04-14 15:59:40 +0200

    adding blogpost

--- vite.config.js
+++ vite.config.js
@@ -20,43 +20,53 @@ export default defineConfig({
       mode: "define",
       items: [
         {
+          title: "April 14th: Modifying the RSS Feed with a Plugin",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
+          pubDate: new Date("2024-04-12T00:00:00Z"),
+        },
+        {
+          title: "April 13th: Introducing GSAP for Animations",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
+          pubDate: new Date("2024-04-12T00:00:00Z"),
+        },
+        {
           title: "April 12th: Refactoring JavaScript Code",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#120424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-12T00:00:00Z"),
         },
         {
           title: "April 11th: Cleaning Up the Code",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#110424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-11T00:00:00Z"),
         },
         {
           title: "April 10th: Styling and Layout Improvements",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#100424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-10T00:00:00Z"),
         },
         {
           title: "April 9th: Adding More Models",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#090424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-09T00:00:00Z"),
         },
         {
           title: "April 8th: Fixing Positioning Issues",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#080424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-08T00:00:00Z"),
         },
         {
           title: "April 5th: First Batch of 3D Models",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#050424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-05T00:00:00Z"),
         },
         {
           title: "April 3rd: Diving Deeper into Three.js",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#030424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-03T00:00:00Z"),
         },
         {
           title: "April 2nd: Starting My 3D Website Journey",
-          link: "https://laurensrousseau.ikdoeict.be/blog/#020424",
+          link: "https://laurensrousseau.ikdoeict.be/blog/",
           pubDate: new Date("2024-04-02T00:00:00Z"),
         },
       ],
